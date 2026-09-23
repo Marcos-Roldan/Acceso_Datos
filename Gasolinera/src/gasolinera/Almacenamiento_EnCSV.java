e2532eebfc669bf74a8b6bfa2a0e9338683d8a52
Alumno Mañana <Alumno Maana@LB-DAM-21.iesluisbraille.es>
2026-09-23 13:07:40 +0200

    Act_Gasolinera 13:07 23/09/2026

--- Gasolinera/src/gasolinera/Almacenamiento_EnCSV.java
+++ Gasolinera/src/gasolinera/Almacenamiento_EnCSV.java
@@ -1,22 +1,100 @@
 package gasolinera;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Almacenamiento_EnCSV implements Almacenamiento {
 
+    Path rutaClientes = Path.of("Cliente.csv");
+    Path rutaPagos = Path.of("Pagos_Repostaje.csv");
+
     @Override
     public List<Cliente> leerClientes() {
-        return List.of();
+        List<Cliente> lista = new ArrayList<>();
+
+        //ABRIMOS EL ARCHIVO EN MODO LECTURA
+        try(BufferedReader in = Files.newBufferedReader(rutaClientes)) {
+
+            String linea = in.readLine(); //Leemos la primera linea
+            while(linea != null) { //Mientras siga habiendo lineas en el archivo
+                if(!linea.isBlank()) { //Comprobamos que la linea no esta vacia ni contiene espacios
+                    String[] datos = linea.split(","); //Cada vez que hay , hay salto de linea
+                    int id = Integer.parseInt(datos[0]); //String a Entero
+                    String nombre = datos[1];
+                    String telefono = datos[2];
+                    String matricula = datos[3];
+
+                    lista.add(new Cliente(id, nombre, telefono, matricula));
+                }
+
+                linea = in.readLine();
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Error al leer el archivo de los clientes");
+        }
+
+        return lista;
     }
 
     @Override
     public boolean escribirCliente(Cliente cliente) {
-        return false;
+        try {
+            Files.createDirectories(rutaClientes.getParent()); //Nos aseguramos de que la carpeta existe, si no existe la crea
+        } catch (IOException ex) {
+            System.out.println("Error al crear el archivo " +ex);
+        }
+
+        try(BufferedWriter out = Files.newBufferedWriter(rutaClientes, StandardCharsets.UTF_8,
+                StandardOpenOption.CREATE, StandardOpenOption.APPEND)) { //CREATE para crear el archivo en caso de no existir, APPEND pone el texto al final
+            String linea = cliente.getId() + "," + cliente.getNombre() + "," + cliente.getTelefono() + "," + cliente.getMatricula(); //Convierto los atributos del objeto cliente en una solo linea separado por ,
+
+            out.write(linea); //Escribimos la linea en el fichero
+            out.newLine(); //Salto de linea para que el siguiente registro se ponga abajo
+
+            return true; //Esta correcto
+        } catch (IOException ex) {
+            System.out.println("Error al escribir el archivo");
+            return false;
+        }
     }
 
     @Override
     public List<Pagos_Repostaje> leerPago() {
-        return List.of();
+        List<Pagos_Repostaje> lista = new ArrayList<>();
+
+        try(BufferedReader in = Files.newBufferedReader(rutaPagos)) {
+
+            String linea = in.readLine();
+            while(linea != null) {
+                if(!linea.isBlank()) {
+                    String[] datos = linea.split(";");
+                    int id = Integer.parseInt(datos[0]);
+                    int id_cliente = Integer.parseInt(datos[1]);
+                    LocalDate fecha = LocalDate.parse(datos[2]);
+                    double litros = Double.parseDouble(datos[3]);
+                    double importe = Double.parseDouble(datos[4]);
+                    String combustible = datos[5];
+
+                    lista.add(new Pagos_Repostaje(id, id_cliente, fecha, litros, importe, combustible));
+                }
+
+                linea = in.readLine();
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Error al leer el archivo de los pagos");
+        }
+
+        return lista;
     }
 
     @Override
